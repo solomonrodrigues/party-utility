b034c92bb00d4e25d016a6daa9ed0cce463d62a9
solomonrodrigues <solomonrodrigues@hotmail.com>
2025-02-26 00:23:38 +1100

    Specify python3.9

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # Install dependencies
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Create staticfiles directory
 mkdir -p staticfiles_build/static
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 
 # Make migrations and apply them
-python manage.py makemigrations
-python manage.py migrate
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
