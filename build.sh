6a08a6fe4f669fb0618051ce76b509e5672d5ea9
solomonrodrigues <solomonrodrigues@hotmail.com>
2025-02-26 01:42:56 +1100

    mkdir

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
 python3 -m pip install --upgrade pip || { echo "pip upgrade failed"; exit 1; }
 python3 -m pip install -r requirements.txt || { echo "requirements install failed"; exit 1; }
+mkdir -p staticfiles_build/static
 python3 manage.py collectstatic --noinput --clear --settings=api.settings.production || { echo "collectstatic failed"; exit 1; }
 echo "BUILD END"
