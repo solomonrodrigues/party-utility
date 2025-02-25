f907fcd82bef500bf6f10f733379160bcefa6c48
solomonrodrigues <solomonrodrigues@hotmail.com>
2025-02-25 23:34:27 +1100

    Call collectstatic on startup

--- api/wsgi.py
+++ api/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+# Run collectstatic on startup
+call_command('collectstatic', '--noinput')
 
 app = get_wsgi_application()
